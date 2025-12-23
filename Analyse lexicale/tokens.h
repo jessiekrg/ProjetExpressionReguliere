38640146ca3499a9ea938e00adb4deabb91cd1a8
Jessica Karega <161818757+jessiekrg@users.noreply.github.com>
2025-12-23 08:50:27 +0100

    tokens

---
+++ Analyse lexicale/tokens.h
@@ -0,0 +1,17 @@
+/* Je définis manuellement les tokens en leur donnant un numéro */
+
+/* Ce fichier permet à lex (notre analyse texte) de savoir si quoi envoyer quand il voit par exemple une partenthèse ouvrante */
+
+#define PAR_O 101
+#define PAR_F 102
+
+#define LTR 106
+#define E 107
+
+#define PLUS 103
+#define ETOILE 104
+#define POINT 105
+
+
+
+
---
+++ IN520_Projet_Python/regexp.y
@@ -0,0 +1,8 @@
+déclarations de variables et de constantes
+%{
+déclarations recopiées dans le fichier toto.yy.c
+%}
+%%
+règles de traduction
+%%
+fonctions auxiliaires en C
